34ac4069af35af4b6227b3f447a6990d33860a74
Jeremy Frandon <42522025+JFrandon@users.noreply.github.com>
2024-12-15 21:00:28 -0500

    revert build

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM alpine:latest
+FROM ubuntu:20.04
 
-RUN apk add bzip2 wget mesa-utils
+RUN apt-get update
+RUN apt-get install --yes bzip2 wget libxext6 libllvm6.0 mesa-utils
 
 COPY build.sh /build.sh
 
--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-sdk_name="renpy-$1-sdk"
+sdk_name=renpy-$1-sdk
 echo "Downloading the specified SDK (${sdk_name})..."
-wget -q "https://www.renpy.org/dl/$1/${sdk_name}.tar.bz2"
+wget -q https://www.renpy.org/dl/$1/${sdk_name}.tar.bz2
 clear
 
 echo "Downloaded SDK version (${sdk_name})."
 echo "Setting up the specified SDK (${sdk_name})..."
-tar -xf "./${sdk_name}.tar.bz2"
+tar -xf ./${sdk_name}.tar.bz2
+rm ./${sdk_name}.tar.bz2
+mv ./${sdk_name} ../renpy
 
 
 
-echo "../${sdk_name}/renpy.sh" "./${sdk_name}/launcher" web_build "$2" --destination "$3"
-"./${sdk_name}/renpy.sh" "./${sdk_name}/launcher" web_build "$2" --destination "$3"
+
+echo ../renpy/renpy.sh ../renpy/launcher web_build "$2" --destination "$3"
+../renpy/renpy.sh ../renpy/launcher web_build "$2" --destination "$3"
